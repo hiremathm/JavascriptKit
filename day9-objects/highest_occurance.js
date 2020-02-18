9f47a4962298ace319968d5ad85d99c1b44ce26c
Shivakumara M <shivasorab@gmail.com>
2020-02-18 08:57:25 +0530

    itteration changes in single loop

--- day9-objects/highest_occurance.js
+++ day9-objects/highest_occurance.js
@@ -3,17 +3,15 @@ let str = 'aaaaaaaaaaaaaaaabbbbbbbbbbccccc'
 function highest_occurance(str){
     let characters = str.split('')
     let output = {}
+    let max = 0;
     for(let i = 0;i < characters.length; i ++){
         if(output[characters[i]] == undefined){
             output[characters[i]] = 1
         }else{
             output[characters[i]] += 1
         }
-    }
-    let max = 0;
-    for(let key in output){
-        if(output[key] > max){
-            max = output[key]
+        if(output[characters[i]] > max){
+            max = output[characters[i]]
         }
     }
     return max;
