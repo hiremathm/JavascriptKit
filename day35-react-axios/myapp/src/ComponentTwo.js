bc0a7fa3fa0dac78281b7947e890cc58cca2eff0
Shivakumara M <shivasorab@gmail.com>
2020-03-17 07:36:30 +0530

    day35-react-axios

---
+++ day35-react-axios/myapp/src/ComponentTwo.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class ComponentTwo extends React.Component {
+	render(){
+		return (
+			<div>
+				<h3>This is from Component Two </h3>
+			</div>
+		)
+	}
+}
+
+export default ComponentTwo;
