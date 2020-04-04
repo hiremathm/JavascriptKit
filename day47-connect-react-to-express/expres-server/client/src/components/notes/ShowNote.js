261b11049a0ba8778e633d2f628fca755def9439
Shivakumara M <shivasorab@gmail.com>
2020-04-04 21:17:51 +0530

    day 47 connect react app to node app

---
+++ day47-connect-react-to-express/expres-server/client/src/components/notes/ShowNote.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import axios from 'axios'
+import {Link} from 'react-router-dom'
+
+class ShowNote extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            note: {}
+        }
+    }
+
+    componentDidMount = () => {
+        const id = this.props.match.params.id
+        axios.get(`http://localhost:3002/notes/${id}`)
+            .then(response => {
+                this.setState({note: response.data})
+            })
+            .catch(error => {
+                console.log("error", error)
+            })
+    }
+
+    handleRemove = () => {
+        const id = this.props.match.params.id
+        axios.delete(`http://localhost:3002/notes/${id}`)
+            .then(response => {
+                this.props.history.push(`/notes`)
+                // this.setState({note: response.data})
+            })
+            .catch(error => {
+                console.log("error", error)
+            })   
+    }
+    render(){
+        return(
+            <div>
+                <h3>Title : {this.state.note.title}</h3>
+                <h5>Body : {this.state.note.body}</h5>
+                <button onClick = {this.handleRemove}>Remove</button>
+                <Link to={`/notes/edit/${this.props.match.params.id}`}><button>Edit</button></Link>
+                <Link to="/notes"><button>Back</button></Link>
+            </div>
+        )
+    }
+}
+
+export default ShowNote;
---
+++ day47-connect-react-to-express/expres-server/index.js
@@ -0,0 +1,112 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+ 
+const app = express()
+const port = 3002
+
+// connect express to mongo via mongoose
+// setup promise library to be ES6 promise
+// javascript has global promise library
+mongoose.Promise = global.Promise
+
+// connect to mongodb
+mongoose.connect('mongodb://127.0.0.1:27017/note-app',{useNewUrlParser: true})
+// it returns promise object, it is asynchronous operation
+    .then(()=>{
+        console.log("Connected to db")
+    })
+    .catch(() => {
+        console.log("Unable to connect to mongodb")
+    })
+
+// schema == object constructor function
+const Schema = mongoose.Schema
+
+const NoteSchema = new Schema({
+    // field configuration
+    title: {type: String, required: true},
+    body: {type: String, required: true},
+    created_at: {type: Date, required: true, default: Date.now()}    
+})
+
+// create model based on schema
+const Note = mongoose.model('Note', NoteSchema)
+
+
+// app listening to port 
+app.listen(port, function(){
+    console.log('listening to port ', port)
+})
+
+
+// confifugre reading incoming json data
+// application level middle ware function
+app.use(express.json())
+
+app.use(cors())
+
+// home page
+app.get('/' , (req, res) =>{
+    res.send('Welcome to noteapp app')
+})
+
+// list notes
+app.get('/notes', (req, res) => { 
+    Note.find()
+        .then((notes) => {
+            res.json(notes)
+        })
+        .catch((error) => {
+            res.json(error)
+        })
+})
+// post notes
+app.post('/notes', (req, res) => { 
+    const body = req.body
+    const note = new Note(body)
+    note.save()
+        .then((note) => {
+            res.json(note)
+        })
+        .catch((error) => {
+            res.json(error)
+        })
+})
+
+// show note
+app.get('/notes/:id', (req, res) => { 
+    const id = req.params.id
+    Note.findById(id)
+        .then((note) => {
+            res.json(note)
+        })
+        .catch((error) => {
+            res.json(error)
+        })
+})
+
+// delete single record
+app.delete('/notes/:id', (req, res) => { 
+    const id = req.params.id
+    Note.findByIdAndDelete(id)
+        .then((note) => {
+            res.json(note)
+        })
+        .catch((error) => {
+            res.json(error)
+        })
+})
+
+// update single record
+app.put('/notes/:id', (req, res) => { 
+    const id = req.params.id
+    const body = req.body
+    Note.findByIdAndUpdate(id,{$set: body},{new: true})
+        .then((note) => {
+            res.json(note)
+        })
+        .catch((error) => {
+            res.json(error)
+        })
+})
